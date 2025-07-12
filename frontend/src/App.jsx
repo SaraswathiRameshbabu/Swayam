46c78b14b70c9fb0a61d9a62c1e40d4457dc9b7a
Saraswathi R <147365655+SaraswathiRameshbabu@users.noreply.github.com>
2025-07-13 00:05:51 +0530

    App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import CreateCourse from './pages/CreateCourse';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-course" element={<CreateCourse />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
